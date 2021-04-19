93a00d926b081d10365ae84115e7a7a161096d88
QendresaS <qendresaskenderi2000@gmail.com>
2021-04-19 15:56:13 +0200

    submitted

---
+++ main.js
@@ -0,0 +1,13 @@
+function initMap() {
+    let ourLocation = {
+        
+    }
+  let map = new google.maps.Map(document.getElementById('map'),{
+    center: ourLocation,
+    zoom: 10
+  });
+
+}
+
+
+
